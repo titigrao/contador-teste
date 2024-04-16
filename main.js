7bf6b4c2c7d6ff9f97c712f38f17d2b8efebd768
Titigas <127964804+titigrao@users.noreply.github.com>
2024-04-16 07:49:41 -0300

    a

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const botoes = document.querySelectorAll(".botao")
 
 for(let i = 0; i<botoes.length; i++){
 	botoes[i].onclick = function(){
-		for (let j = 0;j>botoes.length; j++){
+		for (let j = 0;j<botoes.length; j++){
 			botoes[j].classList.remove('ativo')
 }
-		botoes[j].classList.add('ativo')
+		botoes[i].classList.add('ativo')
 	}
 }
