178a6998747abdc37f7946d1d0e560e86f35e21f
Titigas <127964804+titigrao@users.noreply.github.com>
2024-04-30 07:49:13 -0300

    a

--- main.js
+++ main.js
@@ -16,5 +16,6 @@ for(let i =0;i<botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo = new Date("2024-10-05")
 
 contadores[0].textContent = "Contagem regressiva"
