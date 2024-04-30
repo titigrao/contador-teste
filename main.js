cc4dc803499e12304216c88ac9cf5c4b292be1b0
Titigas <127964804+titigrao@users.noreply.github.com>
2024-04-30 08:04:29 -0300

    A

--- main.js
+++ main.js
@@ -19,4 +19,4 @@ const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date()
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual
+contadores[0].textContent = (tempoObjetivo1 - tempoAtual)
