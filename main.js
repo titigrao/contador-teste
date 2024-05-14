5e98737b2c1e80c5ea945acfe668073eea86e776
Titigas <127964804+titigrao@users.noreply.github.com>
2024-05-14 07:41:56 -0300

    a

--- main.js
+++ main.js
@@ -20,3 +20,14 @@ const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date()
 
 contadores[0].textContent = (tempoObjetivo1 - tempoAtual)/1000/60/60/24
+
+function calculaTempo(tempoObjetivo){
+
+  let tempoAtual = new Date ()
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal/1000);
+  let minutos = Math.floor(segundos/60);
+  let horas = Math.floor(minutos/60);
+  let dias = Math.floor(horas/24);
+return dias;
+}
